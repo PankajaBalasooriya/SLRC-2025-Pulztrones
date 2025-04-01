cff6a81c805d779826c252a4f728f1aedf136d0f
Pankaja Balasooriya <105631765+PankajaBalasooriya@users.noreply.github.com>
2025-04-01 07:51:16 +0530

    Add PCA9548A I2C multiplexer driver implementation

---
+++ software/firmware/SLRC2025-Pulztrones/Debug/Drivers/PCA9548A/Src/subdir.mk
@@ -0,0 +1,27 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+# Toolchain: GNU Tools for STM32 (13.3.rel1)
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+C_SRCS += \
+../Drivers/PCA9548A/Src/PCA9548A.c 
+
+OBJS += \
+./Drivers/PCA9548A/Src/PCA9548A.o 
+
+C_DEPS += \
+./Drivers/PCA9548A/Src/PCA9548A.d 
+
+
+# Each subdirectory must supply rules for building sources it contributes
+Drivers/PCA9548A/Src/%.o Drivers/PCA9548A/Src/%.su Drivers/PCA9548A/Src/%.cyclo: ../Drivers/PCA9548A/Src/%.c Drivers/PCA9548A/Src/subdir.mk
+	arm-none-eabi-gcc "$<" -mcpu=cortex-m4 -std=gnu11 -g3 -DDEBUG -DUSE_HAL_DRIVER -DSTM32F446xx -c -I../Core/Inc -I"C:/Users/PANKAJA/OneDrive/Projects/SLRC/SLRC-2025-Pulztrones/software/firmware/SLRC2025-Pulztrones/Drivers/PCA9548A/Inc" -I"C:/Users/PANKAJA/OneDrive/Projects/SLRC/SLRC-2025-Pulztrones/software/firmware/SLRC2025-Pulztrones/Drivers/SSD1306/Inc" -I"C:/Users/PANKAJA/OneDrive/Projects/SLRC/SLRC-2025-Pulztrones/software/firmware/SLRC2025-Pulztrones/Drivers/VL53L0X/core/inc" -I"C:/Users/PANKAJA/OneDrive/Projects/SLRC/SLRC-2025-Pulztrones/software/firmware/SLRC2025-Pulztrones/Drivers/VL53L0X/platform/inc" -I../Drivers/STM32F4xx_HAL_Driver/Inc -I../Drivers/STM32F4xx_HAL_Driver/Inc/Legacy -I../Drivers/CMSIS/Device/ST/STM32F4xx/Include -I../Drivers/CMSIS/Include -O0 -ffunction-sections -fdata-sections -Wall -fstack-usage -fcyclomatic-complexity -MMD -MP -MF"$(@:%.o=%.d)" -MT"$@" --specs=nano.specs -mfpu=fpv4-sp-d16 -mfloat-abi=hard -mthumb -o "$@"
+
+clean: clean-Drivers-2f-PCA9548A-2f-Src
+
+clean-Drivers-2f-PCA9548A-2f-Src:
+	-$(RM) ./Drivers/PCA9548A/Src/PCA9548A.cyclo ./Drivers/PCA9548A/Src/PCA9548A.d ./Drivers/PCA9548A/Src/PCA9548A.o ./Drivers/PCA9548A/Src/PCA9548A.su
+
+.PHONY: clean-Drivers-2f-PCA9548A-2f-Src
+
